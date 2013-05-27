5b40e8daddd69282f1c5f8d973d087a4f559fc3b
Christian Resma Helle <christian.helle@outlook.com>
2013-05-28 00:12:28 +0200

    Added unit test to verify line endings

---
+++ RESW File Code Generator/ReswCodeGen.Tests/StringExtensionTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using ChristianHelle.DeveloperTools.CodeGenerators.Resw.VSPackage.CustomTool;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ChristianHelle.DeveloperTools.CodeGenerators.Resw.CustomTool.Tests
+{
+    [TestClass]
+    public class StringExtensionTests
+    {
+        [TestMethod]
+        public void LineEndingsAreSame()
+        {
+            uint length;
+            var target = string.Format("First line{0}Second line{0}", Environment.NewLine);
+            var ptr = target.ConvertToIntPtr(out length);
+            var actual = Marshal.PtrToStringAuto(ptr);
+            Assert.AreEqual(target, actual);
+        }
+    }
+}
