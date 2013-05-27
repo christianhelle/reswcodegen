13e20845a23b412d45999a56fd08fe00834d50b7
Christian Resma Helle <christian.helle@outlook.com>
2013-05-28 00:12:08 +0200

    Resolved line endings issue

---
+++ RESW File Code Generator/VSPackage/CustomTool/StringExtension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace ChristianHelle.DeveloperTools.CodeGenerators.Resw.VSPackage.CustomTool
+{
+    public static class StringExtension
+    {
+        public static IntPtr ConvertToIntPtr(this string code, out uint pcbOutput)
+        {
+            var data = Encoding.Default.GetBytes(code);
+            pcbOutput = (uint)data.Length;
+
+            var ptr = Marshal.StringToCoTaskMemAuto(code);
+            return ptr;
+        }
+    }
+}
