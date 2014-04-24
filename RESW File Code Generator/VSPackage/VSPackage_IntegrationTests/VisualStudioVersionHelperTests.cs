cbccee3e751403de320778eb02647d655456f5e5
Christian R. Helle <christian.helle@outlook.com>
2014-04-24 10:02:35 +0200

    Tests

---
+++ RESW File Code Generator/VSPackage/VSPackage_IntegrationTests/VisualStudioVersionHelperTests.cs
@@ -0,0 +1,16 @@
+﻿using ChristianHelle.DeveloperTools.CodeGenerators.Resw.VSPackage.CustomTool;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace VSPackage_IntegrationTests
+{
+    [TestClass]
+    public class VisualStudioHelperTests
+    {
+        [TestMethod]
+        public void GetVersionTests()
+        {
+            var actual = VisualStudioHelper.GetVersion();
+            Assert.AreNotEqual(VisualStudioVersion.Unknown, actual);
+        }
+    }
+}
