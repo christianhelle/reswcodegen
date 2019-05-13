85715e95e3e752ad7b2d912f83199593f0750f60
Christian Helle <christian.helle@outlook.com>
2019-05-13 20:25:36 +0200

    Move VS2013 solution folder into Legacy and rename vs2017 folder to src

---
+++ src/Legacy/ReswCodeGen.Tests/CSharpCodeGeneratorTestsInternal.cs
@@ -0,0 +1,69 @@
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using ChristianHelle.DeveloperTools.CodeGenerators.Resw.VSPackage.CustomTool;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ChristianHelle.DeveloperTools.CodeGenerators.Resw.CustomTool.Tests
+{
+    [TestClass]
+    [DeploymentItem("Resources/Resources.resw")]
+    public class CSharpCodeGeneratorTestsInternal
+    {
+        private string reswFileContents;
+        private const string FILE_PATH = "Resources.resw";
+        private string actual;
+        private ICodeGenerator target;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            reswFileContents = File.ReadAllText(FILE_PATH);
+
+            target = new CodeGeneratorFactory().Create(FILE_PATH.Replace(".resw", string.Empty), "TestApp", reswFileContents, classAccessibility: TypeAttributes.NestedAssembly);
+            actual = target.GenerateCode();
+        }
+
+        [TestMethod]
+        public void GenerateCodeDoesNotReturnNull()
+        {
+            Assert.IsNotNull(actual);
+        }
+
+        [TestMethod]
+        public void GeneratedCodeIsAnInternalClass()
+        {
+            Assert.IsTrue(actual.Contains("internal partial class"));
+        }
+
+        [TestMethod]
+        public void GeneratedCodeContainsPropertiesDefinedInResources()
+        {
+            var resourceItems = target.ResourceParser.Parse();
+
+            foreach (var item in resourceItems.Where(item => !item.Name.Contains(".")))
+                Assert.IsTrue(actual.Contains("public static string " + item.Name));
+        }
+
+        [TestMethod]
+        public void GeneratedCodePropertiesContainsCommentsSimilarToValuesDefinedInResources()
+        {
+            var resourceItems = target.ResourceParser.Parse();
+
+            foreach (var item in resourceItems.Where(item => !item.Name.Contains(".")))
+                Assert.IsTrue(actual.Contains("Localized resource similar to \"" + item.Value + "\""));
+        }
+
+        [TestMethod]
+        public void ClassNameEqualsFileNameWithoutExtension()
+        {
+            Assert.IsTrue(actual.Contains("class Resources"));
+        }
+
+        [TestMethod]
+        public void ResourceLoaderInitializedWithClassName()
+        {
+            Assert.IsTrue(actual.Contains("ResourceLoader.GetForCurrentView(currentAssemblyName + \"/Resources\");"));
+        }
+    }
+}
